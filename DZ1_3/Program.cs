ca631449eea469c8bf3aba328c4f46235e7b9fb5
sergunyaklg <osipov.klg@gmail.com>
2022-10-17 16:38:21 +0400

    DZ 1_3

---
+++ DZ1_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Int32.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Чётное число.");
+}
+else
+{
+   Console.WriteLine("Нечётное число."); 
+}
