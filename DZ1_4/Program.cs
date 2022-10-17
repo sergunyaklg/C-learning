00a06ef0d369ed3719bf19d44a8f84664326df93
sergunyaklg <osipov.klg@gmail.com>
2022-10-17 16:49:04 +0400

    DZ 1_4

---
+++ DZ1_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Int32.Parse(Console.ReadLine());
+
+for (int i = 2; i <= a; i += 2)
+{
+    if (i != 2)
+    {
+        Console.Write(",");
+    }
+    Console.Write($"{i}");
+}
