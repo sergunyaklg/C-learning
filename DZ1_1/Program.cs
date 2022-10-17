36598707444a23857c8577c633a52fb907dfcd2e
sergunyaklg <osipov.klg@gmail.com>
2022-10-17 16:14:29 +0400

    DZ 1_1

---
+++ DZ1_1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Int32.Parse(Console.ReadLine());
+
+int maxnumber;
+int minnumber;
+
+// int maxnumber = Math.Max(a, b);
+// int minnumber = Math.Min(a, b);
+
+if ( a > b ) 
+{
+   maxnumber = a;
+   minnumber = b;
+} 
+else 
+{
+   maxnumber = b;
+   minnumber = a;
+}
+
+
+Console.WriteLine($"Максимальное число: {maxnumber}. Минимальное число: {minnumber}");
