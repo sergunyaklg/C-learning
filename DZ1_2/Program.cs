deaef509bc5deee2b9886db772d4d721c48b042c
sergunyaklg <osipov.klg@gmail.com>
2022-10-17 16:33:17 +0400

    DZ 1_2

---
+++ DZ1_2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int c = Int32.Parse(Console.ReadLine());
+
+int maxnumber = a;
+
+// Math.max(a,b)
+if (b > a)
+{
+    maxnumber = b;
+}
+
+// Math.max(c, Math.max(a,b)) === Math.max(a,b,c)
+if (c > a)
+{
+    maxnumber = c;
+}
+
+Console.WriteLine($"Максимальное число: {maxnumber}.");
