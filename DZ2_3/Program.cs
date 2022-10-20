d4acc9aee4c15056e61e31aced210b2611ba843c
sergunyaklg <osipov.klg@gmail.com>
2022-10-20 17:24:25 +0400

    Задача 15

---
+++ DZ2_3/Program.cs
@@ -0,0 +1,15 @@
+﻿void DayOfWeek(int day)
+{
+if (day > 5)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+}
+
+Console.WriteLine("Введите число дня недели, чтоб узнать выходной ли это день: ");
+int day = Int32.Parse(Console.ReadLine());
+DayOfWeek(day);
