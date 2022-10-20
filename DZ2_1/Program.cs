b546ecd6ed5a11cae2d3df8570c3acc38a4bdbad
sergunyaklg <osipov.klg@gmail.com>
2022-10-20 15:32:04 +0400

    Задача 10

--- DZ2_1/Program.cs
+++ DZ2_1/Program.cs
@@ -1,1 +1,13 @@
-﻿//C
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+
+int SecondDigit(int Number)
+{
+    int secondDigit = (Number / 10) % 10;
+    return secondDigit;
+}
+
+Console.WriteLine("Введите трехзначное число: ");
+int Number = Int32.Parse(Console.ReadLine());
+
+int secondDigit = SecondDigit(Number);
+Console.WriteLine("Вторая цифра: " + secondDigit);
