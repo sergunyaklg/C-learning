5d8a9f5e095be1a636ee6e47387c333cf2637787
sergunyaklg <osipov.klg@gmail.com>
2022-10-20 16:52:20 +0400

    Задача 13

---
+++ DZ2_2/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.Через строку решать нельзя.
+void FindThirdDigit(int Number)
+{
+    int quantityOfDigits = 0;
+    int Number1 = Number;
+    while(Number1>0) 
+    {
+        quantityOfDigits++;
+        Number1/=10;
+    }
+
+    if(quantityOfDigits > 2) 
+    {
+        int remain = Number / ((int)Math.Pow(10,(quantityOfDigits-3))) % 10;
+        Console.WriteLine("Третья цифра: " + remain);
+    }
+    else 
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int Number = Int32.Parse(Console.ReadLine());
+FindThirdDigit(Number);
