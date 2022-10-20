b5d1c205628127ee8e67939f8a7097eab51713af
sergunyaklg <osipov.klg@gmail.com>
2022-10-20 17:37:14 +0400

    Задача необязательная 2

---
+++ DZ2_4/Program.cs
@@ -0,0 +1,22 @@
+﻿void Programmer(int quantity)
+{
+    if ((quantity % 100)/10 == 1)
+    {
+        Console.WriteLine("Программистов");
+    }
+    else if (quantity % 10 == 1)
+    {
+        Console.WriteLine("Программист");
+    }
+    else if (quantity % 10 > 1 && quantity % 10 < 5)
+    {
+        Console.WriteLine("Программиста");
+    }
+    else
+    {
+        Console.WriteLine("Программистов");
+    }
+}
+Console.WriteLine("Введите количество программистов: ");
+int quantity = Int32.Parse(Console.ReadLine());
+Programmer(quantity);
