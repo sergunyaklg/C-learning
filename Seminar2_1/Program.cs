daf76b98ffd10bfd209f22ab99af416f67e08e81
sergunyaklg <osipov.klg@gmail.com>
2022-10-20 15:12:30 +0400

    RandomNumber + MaxDigit

--- Seminar2_1/Program.cs
+++ Seminar2_1/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿int GenerateRandomNumber() 
+{
+    return new Random().Next(10,100);
+}
+
+int GetMaxDigitForDigitsNumber(int randomNumber)
+{
+    int firstDigit = randomNumber / 10;
+    int secondDigit = randomNumber % 10;
+    Console.WriteLine("Сгенерировали число в диапазоне [10, 99]: " + randomNumber);
+    int max = firstDigit;
+    if (secondDigit > firstDigit)
+    {
+        max = secondDigit;
+    }
+    return max;
+}
+
+int randomNumber = GenerateRandomNumber();
+int max=GetMaxDigitForDigitsNumber(randomNumber);
+Console.WriteLine("Наибольшая цифра числа: " + max);
