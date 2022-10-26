21b1d90e06a672d11f613e4a1611464b270cc065
Sergei Osipov <osipov.klg@gmail.com>
2022-10-26 18:36:26 +0400

    расстояние

---
+++ DZ3_2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в N-мерном пространстве. 
+// Сначала задается N с клавиатуры, потом задаются координаты точек.
+
+Console.WriteLine("Введите размерность пространства N: ");
+int N = int.Parse(Console.ReadLine()!);
+int[] dotA = new int [N];
+int[] dotB = new int [N];
+
+for (int i = 0; i < N; i++)
+{
+    Console.WriteLine($"Введите {i}-ую координату точки A ():");
+    dotA[i] = int.Parse(Console.ReadLine()!);
+}
+
+for (int i = 0; i < N; i++)
+{
+    Console.WriteLine($"Введите {i}-ую координату точки B ():");
+    dotB[i] = int.Parse(Console.ReadLine()!);
+}
+
+void Distance(int[] dotA, int[] dotB)
+{
+    double SumOfSquares = 0;
+    for (int i = 0; i < N; i++)
+    {
+        SumOfSquares += Math.Pow(dotA[i] - dotB[i], 2); 
+    }
+
+    double distance = Math.Sqrt(SumOfSquares);
+    Console.WriteLine($"Расстояние между точками: {distance}");
+}
+
+Distance(dotA, dotB);
+
+
