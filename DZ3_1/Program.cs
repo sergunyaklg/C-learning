d8b2e45ccd955201fde64fe97a29fb31425e3151
Sergei Osipov <osipov.klg@gmail.com>
2022-10-26 18:36:00 +0400

    палиндром

---
+++ DZ3_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход целое число любой разрядности и проверяет, является ли оно палиндромом. Через строку нельзя решать само собой.
+
+bool isPolindrom(int x) {
+    int y = 0;
+    int z = x;
+    while (z > 0) {
+        y = y * 10 + z % 10;
+        z /= 10;
+    }
+    return x == y;
+}
+
+
+Console.WriteLine("Введите число для проверки на палиндром: ");
+if (isPolindrom(int.Parse(Console.ReadLine()))) {
+    Console.WriteLine("Да");
+} else {
+    Console.WriteLine("Нет");
+}
