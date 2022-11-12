6079ae9108d37c4f869781911f1b0f554698feaa
Sergei Osipov <osipov.klg@gmail.com>
2022-11-12 20:18:49 +0300

    Задача 25

---
+++ DZ4_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите функцию, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+void DegradingAinB(int A, int B)
+{
+    double result = Math.Pow(A, B);
+    Console.WriteLine("Результат: " + result);
+}
+
+
+
+Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+
+DegradingAinB(A, B);
