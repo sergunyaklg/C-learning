3a57611a0c785de668fc80f7f42254865805f186
Sergei Osipov <osipov.klg@gmail.com>
2022-11-12 20:26:17 +0300

    Задача 27

---
+++ DZ4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Через строку решать нельзя.
+
+void SumOfDigits(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    Console.WriteLine("Сумма цифр в числе: " + sum);
+}
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+SumOfDigits(number);
