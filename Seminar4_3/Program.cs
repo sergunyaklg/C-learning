0237393030230a41f2d0e67393ceeb6093bbebf8
Sergei Osipov <osipov.klg@gmail.com>
2022-11-12 13:02:25 +0300

    Finish

---
+++ Seminar4_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдает сумму чисел от 1 до А.
+
+int GetSumFrom1ToN(int n)
+{
+    int result = 0;
+    int count = 0;
+    while (count < n)
+    {
+        count++;
+        result += count;
+    }
+    return result;
+}
+
+Console.WriteLine("Прошграмма расчёт суммы чисел от 1 до N");
+Console.WriteLine("Введите число N:");
+int number = int.Parse(Console.ReadLine()!);
+
+int result = GetSumFrom1ToN(number);
+
+Console.WriteLine("Результат:" + result);
