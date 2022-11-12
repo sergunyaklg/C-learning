ffacb03f4dc5452520f0c95a3a77fc07d2ee3afd
Sergei Osipov <osipov.klg@gmail.com>
2022-11-12 13:51:50 +0300

    finish

---
+++ Seminar4_5/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+
+Console.WriteLine("Введите число N: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int FactorialDigit(int number)
+{
+    int count = 1;
+    int result = 1;
+    while (count <= number)
+    {
+        result = result * count;
+        count++;
+    }
+    return result;
+}
+
+int result = FactorialDigit(number);
+
+Console.WriteLine("Результат: " + result);
