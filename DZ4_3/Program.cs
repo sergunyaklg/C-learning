947fb6bbaf9aeb5f1f023d9aa24779f479862d58
Sergei Osipov <osipov.klg@gmail.com>
2022-11-12 20:38:21 +0300

    Задача 29

---
+++ DZ4_3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая задаёт массив из 8 целых чисел с клавиатуры и далее выводит массив на экран в одну строку.
+
+void CreatArrayWithNumbers (int numbers = 8)
+{
+    int[] input = new int[numbers];
+    for (int i = 0; i < numbers; i++)
+    {
+        Console.WriteLine("Введите число:");
+        input[i] = int.Parse(Console.ReadLine()!);
+    }
+    Print(input);
+}
+
+void Print(int[] input)
+{
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (i == input.Length - 1)
+        {
+            Console.Write(input[i]);
+        }
+        else
+        {
+            Console.Write(input[i] + ", ");
+        }
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите 8 чисел:");
+CreatArrayWithNumbers();
