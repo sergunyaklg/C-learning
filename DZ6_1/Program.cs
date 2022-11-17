e6edcb6090040e11a6c817a076de25ad989e36b6
Sergei Osipov <osipov.klg@gmail.com>
2022-11-17 18:25:32 +0300

    Задача 41

---
+++ DZ6_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void CountPositiveNumbers(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            result ++;
+        }
+    }
+    Console.WriteLine($"Количество чисел больше нуля: {result}");
+}
+
+Console.Write("Введите числа через запятую: ");
+int[] array = Console.ReadLine().Split(", ").Select(x=>int.Parse(x)).ToArray();
+CountPositiveNumbers(array);
