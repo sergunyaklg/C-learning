8fbb18961526b4c2bbb484b048975bc560e51d15
Sergei Osipov <osipov.klg@gmail.com>
2022-11-17 18:40:28 +0300

    Задача 43

---
+++ DZ6_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+void FindPointIntersection (double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения: ({x}, {y})");
+}
+
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine());
+
+
+FindPointIntersection(b1, k1, b2, k2);
